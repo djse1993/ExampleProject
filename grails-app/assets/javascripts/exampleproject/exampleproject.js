150bdbdae78df1b5d0787ed5364e5cd55d8ea16b
David Sanchez <djse1993@gmail.com>
2016-06-24 13:14:37 -0400

    Uploading Example project using Angular Profile 3.1.8

---
+++ grails-app/assets/javascripts/exampleproject/exampleproject.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /exampleproject/core/exampleproject.core
+//= require /exampleproject/index/exampleproject.index
+
+angular.module("exampleproject", [
+    "exampleproject.core",
+    "exampleproject.index"
+]);
---
+++ grails-app/domain/domains/User.groovy
@@ -0,0 +1,10 @@
+package domains
+
+class User {
+    String name
+    String lastName
+    String password
+    String email
+    static constraints = {
+    }
+}
